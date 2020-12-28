7330c8545ccd2857aa7948852fd9eeaa4f3db4fd
jinah <jinah0818@gmail.com>
2020-12-28 19:56:24 +0900

    add Decorators and query

--- hi-nest/src/movies/movies.controller.ts
+++ hi-nest/src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,24 +8,33 @@ export class MoviesController {
         return 'This will return all movies.';
     }
 
-    @Get('/:id')
+    
+    @Get('search')
+    search(@Query('year') serachingYear){
+        return `we are searching the movie made after : ${serachingYear}`;
+    }
+
+    @Get(':id')
     getOne(@Param("id") id: string){
         return `This will return one movie with ID : ${id}`;
     }
 
     @Post()
-    create(){
-        return 'This will create a Movie.'
+    create(@Body() movieData){
+        return movieData;
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') id: string){
         return `This will delete a movie with ID : ${id}`
     }
 
-    @Patch('/:id')  //리소스의 일부분만 업데이트
-    patch(@Param('id') id:string){
-        return `This will patch a movie with ID : ${id}`
+    @Patch(':id')  //리소스의 일부분만 업데이트
+    patch(@Param('id') id:string, @Body() updateData){
+        return {
+            updatedMovie: id,
+            ...updateData
+        }
     }
 
      
